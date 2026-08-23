203d9b873b7bff56734ac2110dfcb9d66a05431b
furugenminamifc <furugen.minamifc@gmail.com>
2026-08-23 18:46:36 +0900

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,7 +2,6 @@
 // Supabaseの Project URL と anon public key を入力してください。
 // ※ service_role key は絶対にここへ入れないでください。
 window.FURUGEN_SUPABASE = {
-  url: "=https://lxpfdmyodldctdvfzjgw.supabase.co  anonKey: "",
+  url: "=https://lxpfdmyodldctdvfzjgw.supabase.co  anonKey: "sb_publishable_sJ8UlKYktXsFLwBWd3P7WA_kwwO9QNE,
   storageBucket: "member-photos"
 };
-sb_publishable_sJ8UlKYktXsFLwBWd3P7WA_kwwO9QNE
